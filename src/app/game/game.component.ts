4292b68ab23c58f3a75703d02c5ff0caea908fd9
Ali Bayati <bayati.ali@icloud.com>
2018-06-28 13:41:01 -0500

    second

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
 import { Socket } from 'ng-socket-io';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-game',
@@ -9,9 +11,13 @@ import { Socket } from 'ng-socket-io';
 })
 export class GameComponent implements OnInit {
 
-  constructor(private _httpService:HttpService ,private _socket:Socket) { }
+  constructor(private _httpService:HttpService ,private _socket:Socket,private _route:Router) {
+
+    
+   }
   currentUser;
   table=[];
+  hand;
   ngOnInit() {
    
     let obs = this._httpService.isLogged();
@@ -21,11 +27,23 @@ export class GameComponent implements OnInit {
       // this.currentUser=JSON.parse(data['_body']).user['first_name']
       console.log("the user in game",this.currentUser)
       this._socket.emit('new_game',this.currentUser)
+
+      this._socket.on('load_user',function(user){
+        
+        // console.log("printing user name",user.first_name)
+        console.log("printing user hand",user.hand)
+        this.hand=user.hand;
+        console.log("the user in game after the socket update",this.hand)
+
+        // console.log("printing currentUser hand",this.currentUser)
+
+
     })
-    this._socket.on('load_user',function(user){
-      console.log("printing user name",user.first_name)
-      console.log("printing user hand",user.hand)
+
+      
+      
     })
+    
   }
   
 
